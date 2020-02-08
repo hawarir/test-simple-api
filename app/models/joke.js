e2418cc84a8e0815b0b5b3a85871f47d02137cfb
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-08 09:52:19 +0700

    Split words: Lowercase all letters and remove non alphanumeric

--- app/models/joke.js
+++ app/models/joke.js
@@ -1,5 +1,5 @@
 module.exports = function main(Joke) {
   Joke.prototype.words = function words() {
-    return this.text.split(' ');
+    return this.text.split(' ').map((item) => item.toLowerCase().replace(/\W/g, ''));
   };
 };
